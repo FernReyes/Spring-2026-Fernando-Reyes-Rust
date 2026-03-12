cb33780963d4df1deeb63a84205dca8dea53290d
FernReyes <fernando.reyes05@utrgv.edu>
2026-03-12 02:41:57 +0000

    Bank Account

--- bank_account/src/main.rs
+++ bank_account/src/main.rs
@@ -38,23 +38,62 @@ mod tests {
     #[test]
     fn test_new_account() {
         // Write a test for creating a new account
+        let account_test = BankAccount::new(300.0);
+        assert_eq!(account_test.balance(), 300.0);
     }
 
     #[test]
     fn test_deposit() {
         // Write a test for depositing money
+        let mut account_test = BankAccount::new(600.0);
+        account_test.deposit(70.0);
+        assert_eq!(account_test.balance(), 670.0);
+        
     }
 
     #[test]
     fn test_withdraw() {
         // Write a test for withdrawing money
+        let mut account_test = BankAccount::new(450.0);
+        account_test.withdraw(40.0);
+        assert_eq!(account_test.balance(), 410.0);
     }
 
-    // Add more tests here
+    #[test]
+    fn check_on_balance() {
+        // Write a test for depositing money
+        let mut account_test = BankAccount::new(100.0);
+        account_test.deposit(200.0);
+        account_test.withdraw(150.0);
+        assert_eq!(account_test.balance(), 150.0);
+    }
+
+    //Edge tests
+    #[test]
+    fn negative(){  //Test for negative numbers in both deposit and withdraw.
+        let mut account_test = BankAccount::new(50.0);
+        account_test.deposit(-20.0);
+        account_test.withdraw(-40.0);
+        assert_eq!(account_test.balance(), 50.0);
+    }
+
+    #[test]
+    fn large_withdraw(){
+        let mut account_test = BankAccount::new(200.0);
+        account_test.withdraw(300.0);
+        assert_eq!(account_test.balance(), 200.0);
+    }
 }
 
 fn main() {
     
-    let _account = BankAccount::new(500.0);  //Variable account is sent to new function in struct.
+    let mut _account = BankAccount::new(500.0);  //Variable account is sent to new function in struct.\
+    println!("Original balance is {}", _account.balance());
+
+    _account.deposit(300.0);
+    _account.withdraw(900.0);
+    _account.withdraw(-500.0);
+    _account.withdraw(400.0);
+    println!("New balance is {}", _account.balance());
 
 }
